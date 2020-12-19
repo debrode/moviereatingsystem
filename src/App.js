400752c96d6ada3e4d530b5daeb5bffda6f3e85e
Debarun Roy <debarun.dr7.roy@gmail.com>
2020-12-19 20:14:59 +0530

    fix : file org

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
-function App() {
+function App(){
+  const sayHello=()=>{
+    console.log("Hello");
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+      <button onClick={sayHello}>hello</button>
     </div>
   );
 }
+
 
 export default App;
