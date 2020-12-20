b2584af5d16d464ba7c336867818fa0a57a51ac0
Debarun Roy <debarun.dr7.roy@gmail.com>
2020-12-21 00:29:56 +0530

    Style : added a header icon

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -16,7 +16,7 @@ const setVoteClass =(vote)=>{
 
 const Movie=({title,poster_path,overview,vote_average})=>(
    <div className="movie">
-       <img src={img_api+poster_path} alt={title}/>
+       <img src={(poster_path)?(img_api+poster_path):"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fmovie&psig=AOvVaw3A-h-P02VMiPewNKnMv4tY&ust=1608576932050000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICTlpye3e0CFQAAAAAdAAAAABAJ"} alt={title}/>
        <div className="movie-info">
            <h3>{title}</h3>
            <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
