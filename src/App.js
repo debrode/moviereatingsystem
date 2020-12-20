5b38e77ded492ef48a6ddf2b19b051c16063de23
Debarun Roy <debarun.dr7.roy@gmail.com>
2020-12-21 00:24:27 +0530

    Style: Designed whole Feat: added hooks for search and feed

--- src/App.js
+++ src/App.js
@@ -4,30 +4,61 @@ import Movie from './components/Movie';
 
 const featured_api="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-const search_api="https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query";
+const search_api="https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 
 function App(){
   const [movies,setMovies]= useState([]);
+
+  const [searchterm, setsearchterm] = useState('');
   useEffect( ()=>{
     fetch(featured_api).then(res=>res.json())
     .then(data=>{
-      console.log(data);
-      setMovies(data.results);
+     setMovies(data.results);
     })
     
   },[])
+
+  const handleOnSubmit = (e) =>{
+    e.preventDefault();
+     if(searchterm){
+    fetch(search_api+searchterm).then((res)=>res.json()).then((data)=>{if(data.results){
+    setMovies(data.results)}});
+    setsearchterm('');
+   }
+
+  };
+
+  const handleOnChange = (e) => {
+    setsearchterm(e.target.value);
+  }
+
   return (
     <>
     <header>
-    <input className="search" type="text" placeholder="Search..."/>
+      <form onSubmit={handleOnSubmit}>
+      <input 
+      className="search" 
+      type="text" 
+      placeholder="Search..."
+      value={searchterm}
+      onChange={handleOnChange}
+      />
+      </form>
+    
   </header>
     <div className="movie-container">
+     
+        {movies.length>0 
+        &&movies.map(movie =>
+        <Movie key={movie.id} {...movie}
+        />)
+}
+
    
-     {movies.length>0 &&
-     movies.map(movie=>
-        <Movie key={movie.id} {...movie}/>)}
     </div>
+
+
 
     </>
       );
