a6e66231acf16ee80f1d29b2b9a4e232cd64f9fc
Debarun Roy <debarun.dr7.roy@gmail.com>
2020-12-20 22:22:53 +0530

    feat & style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
-import './App.css';
+import React, {useEffect,useState} from "react";
+// import './App.css';
+import Movie from './components/Movie';
+
+const featured_api="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const search_api="https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query";
+
+
 function App(){
-  const sayHello=()=>{
-    console.log("Hello");
-  }
- 
+  const [movies,setMovies]= useState([]);
+  useEffect( ()=>{
+    fetch(featured_api).then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+      setMovies(data.results);
+    })
+    
+  },[])
   return (
-    <div>
-      <h1>Hello world</h1>
-      <button onClick={sayHello}>hello</button>
+    <>
+    <header>
+    <input className="search" type="text" placeholder="Search..."/>
+  </header>
+    <div className="movie-container">
+   
+     {movies.length>0 &&
+     movies.map(movie=>
+        <Movie key={movie.id} {...movie}/>)}
     </div>
-  );
+
+    </>
+      );
 }
 
 
